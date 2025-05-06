9c27aa44646419b1b4770b600d2e18f534e8671a
olshanskiyvv <90987935+olshanskiyvv@users.noreply.github.com>
2025-05-06 20:59:02 +0300

    add type hints to dao base

--- app/dao/base.py
+++ app/dao/base.py
@@ -1,35 +1,37 @@
+from typing import Sequence, Optional
+
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.future import select
 from sqlalchemy import update as sqlalchemy_update, delete as sqlalchemy_delete
 from app.database import async_session_maker
 
 
-class BaseDAO:
-    model = None
+class BaseDAO[T]:
+    model: T = None
 
     @classmethod
-    async def find_all(cls):
+    async def find_all(cls) -> Sequence[T]:
         async with async_session_maker() as session:
             query = select(cls.model)
             result = await session.execute(query)
             return result.scalars().all()
 
     @classmethod
-    async def find_one_or_none_by_id(cls, data_id: int):
+    async def find_one_or_none_by_id(cls, data_id: int) -> Optional[T]:
         async with async_session_maker() as session:
             query = select(cls.model).filter_by(id=data_id)
             result = await session.execute(query)
             return result.scalar_one_or_none()
 
     @classmethod
-    async def find_one_or_none(cls, **filter_by):
+    async def find_one_or_none(cls, **filter_by) -> Optional[T]:
         async with async_session_maker() as session:
             query = select(cls.model).filter_by(**filter_by)
             result = await session.execute(query)
             return result.scalar_one_or_none()
 
     @classmethod
-    async def add(cls, **values):
+    async def add(cls, **values) -> T:
         async with async_session_maker() as session:
             async with session.begin():
                 new_instance = cls.model(**values)
@@ -42,7 +44,7 @@ class BaseDAO:
                 return new_instance
 
     @classmethod
-    async def update(cls, filter_by, **values):
+    async def update(cls, filter_by, **values) -> int:
         async with async_session_maker() as session:
             async with session.begin():
                 query = (
@@ -60,7 +62,7 @@ class BaseDAO:
                 return result.rowcount
 
     @classmethod
-    async def delete(cls, delete_all: bool = False, **filter_by):
+    async def delete(cls, delete_all: bool = False, **filter_by) -> int:
         if not delete_all and not filter_by:
             raise ValueError("Необходимо указать хотя бы один параметр для удаления.")
 

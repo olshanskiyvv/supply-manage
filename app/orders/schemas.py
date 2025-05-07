ebee1bcbfb57012a7ffde8e39d50e78984efec6e
olshanskiyvv <90987935+olshanskiyvv@users.noreply.github.com>
2025-05-07 20:23:25 +0300

    add schemas for order moduls

---
+++ app/orders/schemas.py
@@ -0,0 +1,34 @@
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+from app.auth.schemas import SUser
+from app.orders.models import Status
+from app.suppliers.schemas import SSupplier
+
+
+class SOrder(BaseModel):
+    id: int = Field(..., description='Идентификатор')
+    number: UUID = Field(..., description='Номер заказа')
+    status: Status = Field(..., description='Статус заказа')
+    cancel_comment: str = Field('', description='Комментарий к отмене')
+    supplier: SSupplier = Field(..., description='Поставщик')
+
+class SOrderAdmin(SOrder):
+    user: SUser = Field(..., description='Пользователь')
+
+class SOrderRB(BaseModel):
+    supplier_id: int = Field(..., description='Идентификатор поставщика')
+
+class SOrderProductRB(BaseModel):
+    product_id: int = Field(..., description='Идентификатор товара')
+    amount: int = Field(..., ge=1, description='Количество')
+
+class SProductShort(BaseModel):
+    product_id: int = Field(..., description='Идентификатор товара')
+    title: str = Field(..., description='Наименование')
+    amount: int = Field(..., ge=1, description='Количество')
+
+class SFullOrder(SOrder):
+    products: list[SProductShort] = Field(..., description='Товары')
+

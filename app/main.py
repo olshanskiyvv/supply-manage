3b4ca8c21686826b9d4ec6107a89507e18a0c6c3
olshanskiyvv <90987935+olshanskiyvv@users.noreply.github.com>
2025-05-07 21:45:39 +0300

    add order routes to main

--- app/main.py
+++ app/main.py
@@ -3,11 +3,13 @@ from fastapi import FastAPI
 from app.auth.api import router as auth_router
 from app.products.api import router as products_router
 from app.suppliers.api import router as suppliers_router
+from app.orders.api import router as orders_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
 app.include_router(products_router)
 app.include_router(suppliers_router)
+app.include_router(orders_router)
 
 
